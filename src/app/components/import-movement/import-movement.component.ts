b51b13ab2535706b541a8cbb0e2defff1f20bbbd
porfirioR <porfirioperezgon@gmail.com>
2022-08-26 14:42:29 -0400

    import read content

--- src/app/components/import-movement/import-movement.component.ts
+++ src/app/components/import-movement/import-movement.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportMovementComponent implements OnInit {
   protected fileName!: string
+  protected file!: File
 
   constructor(private readonly location: Location) { }
+  
   ngOnInit() {
   }
 
@@ -18,14 +20,30 @@ export class ImportMovementComponent implements OnInit {
   }
 
   protected onFileSelected = (event: Event) => {
-    const input =(event.target) as HTMLInputElement
+    const input = (event.target) as HTMLInputElement
     const file = input.files?.item(0)
-    if (file) {
-      this.fileName = file.name;
+    if (file && file.type == 'text/csv') {
+      this.file = file;
     //   const formData = new FormData();
     //   formData.append("thumbnail", file);
     //   const upload$ = this.http.post("/api/thumbnail-upload", formData);
     //   upload$.subscribe();
     }
+  }
+
+  protected processFile = () => {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event: ProgressEvent<FileReader>) => {
+      const result = event?.target?.result;
+      // Do something with result
+    });
+  
+    reader.addEventListener('progress', (event) => {
+      if (event.loaded && event.total) {
+        const percent = (event.loaded / event.total) * 100;
+        console.log(`Progress: ${Math.round(percent)}`);
+      }
+    });
+    reader.readAsDataURL(this.file);
   }
 }
