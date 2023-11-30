2d60b56342768285965d385f65bc4156c01e0fcd
PorfirioR <porfirioperezgon@gmail.com>
2023-11-30 00:45:03 -0300

    7-test-end-to-end

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { UserDataModel } from '../../models/user-data.model';
 import { ConfigurationModel } from '../../models/configuration.model';
 import { LanguageType } from '../../enums/language-type.enum';
 import { NumberType } from '../../enums/number-type.enum';
+import { environment } from 'src/environments/environment';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +16,18 @@ import { NumberType } from '../../enums/number-type.enum';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  protected isLocal = !environment.production
+  protected formGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
   constructor(private authService: AuthService, private router: Router, private readonly userService: UserService) {}
 
   ngOnInit() {
-    this.login()
+    if(!this.isLocal) {
+      this.login()
+    }
   }
 
   private login = () => {
@@ -37,4 +47,22 @@ export class LoginComponent implements OnInit {
     })
     .catch((x) => console.error(x))
   };
+
+  protected loginUser = () => {
+    this.authService.signInUser(this.formGroup.value.email!, this.formGroup.value.password!)
+    .then((x: UserCredential) => {
+      const userData: UserDataModel = {
+        email: x.user.email!,
+        activeCategories: [],
+        allCategories: [],
+        userCategories: [],
+        photo: x.user.photoURL,
+        displayName: x.user.displayName!,
+        userConfiguration: new ConfigurationModel(LanguageType.English, NumberType.English, x.user.email!)
+      }
+      this.userService.setUser(userData)
+      this.router.navigate([''])
+    })
+    .catch((x) => console.error(x))
+  }
 }
