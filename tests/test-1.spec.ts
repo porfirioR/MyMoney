3f137c127fc4f71ef6e4100a2a6713a12c9636c3
PorfirioR <porfirioperezgon@gmail.com>
2024-01-29 00:27:39 -0300

    7-test-end-to-end

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test.skip('test', async ({ page }) => {
-await page.locator('article').filter({ hasText: 'highlight_off fastfood2yN9zpP3M4 (user)edit' }).getByRole('button').first().click();
-await page.getByRole('button', { name: 'Delete' }).click();
-});
